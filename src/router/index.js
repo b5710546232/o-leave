5745d24c0315701e02064966a10cc37c256120b1
kasiditp <kasidit.p@ku.th>
2018-05-05 18:20:11 +0700

    edit route for all user

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import LoginPage from '@/pages/LoginPage'
+import AdminPage from '@/pages/AdminPage'
+import SubordinatePage from '@/pages/SubordinatePage'
+import SupervisorPage from '@/pages/SupervisorPage'
 
 Vue.use(Router)
 
@@ -10,6 +13,21 @@ export default new Router({
       path: '/',
       name: 'LoginPage',
       component: LoginPage
+    },
+    {
+      path: '/admin',
+      name: 'AdminPage',
+      component: AdminPage
+    },
+    {
+      path: '/subordinate',
+      name: 'SubordinatePage',
+      component: SubordinatePage
+    },
+    {
+      path: '/supervisor',
+      name: 'SupervisorPage',
+      component: SupervisorPage
     }
   ]
 })
