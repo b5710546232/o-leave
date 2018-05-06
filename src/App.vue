b38ea5657ee6416030954c5a2e367c442cfc4cee
b5710546232 <nattapat.su@ku.th>
2018-05-06 16:11:57 +0700

    login page

--- src/App.vue
+++ src/App.vue
@@ -18,6 +18,13 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
+  width: 100%;
+  height:100%;
+}
+html, body{
+  width: 100%;
+  height:100%;
+  margin:0;
 }
 </style>
--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,19 +1,31 @@
 <template>
     <div class='root'>
-        <div class='container'>
-            <div>
-                <el-input placeholder="Please input" v-model="msg"></el-input>
-            </div>
-        </div>
+        <el-card class="box-card login-box">
+            <h1>Oleave</h1>
+            <el-form :model="ruleFormLogin" status-icon :rules="rules2" ref="ruleFormLogin" label-width="120px" class="login-form" :label-position="'top'">
+                <el-form-item label="Email" prop="pass">
+                    <el-input type="text" v-model="ruleFormLogin.pass" auto-complete="off">
+                    </el-input>
+                </el-form-item>
+                <el-form-item label="Password" prop="checkPass">
+                    <el-input type="password" v-model="ruleFormLogin.checkPass" auto-complete="off">
+                    </el-input>
+                </el-form-item>
+                <el-form-item>
+                    <el-button type="primary" @click="submitForm('ruleFormLogin')">Log in</el-button>
+                </el-form-item>
+            </el-form>
+        </el-card>
     </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  data () {
+  name: "HelloWorld",
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js Login'
+      msg: "Welcome to Your Vue.js Login",
+      ruleFormLogin: {}
     }
   }
 }
@@ -21,5 +33,22 @@ export default {
 
 <!-- Add 'scoped' attribute to limit CSS to this component only -->
 <style scoped>
-
+.root {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+}
+.login-box {
+  width: 40%;
+  height: auto;
+  display: flex;
+  justify-content: center;
+}
+@media only screen and (max-width: 425px) {
+    .login-box {
+        width: 90%;
+    }
+}
 </style>
