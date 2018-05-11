685a2e34e00d9cf7b1009c491d1155d9a674b5f7
b5710546232 <nattapat.su@ku.th>
2018-05-11 19:08:16 +0700

    add addueserpage

--- src/api/base.js
+++ src/api/base.js
@@ -1,7 +1,9 @@
 import Axios from 'axios'
 import {getAccessToken} from './user'
-export const baseURL = `https://limitless-falls-39048.herokuapp.com/api`
 export const URL = `https://limitless-falls-39048.herokuapp.com/`
+export const baseURL = `${URL}/api`
+// `http://vleave-back.ivaa.space`
+// `https://limitless-falls-39048.herokuapp.com/`
 // const baseURL = 'http://localhost/tender_bartik/public/api '
 export const DefaultHeaders = {
   'Content-Type': 'application/json'
