6807740d6095a9f21fe85e1cbd73744a0b95d8eb
b5710546232 <nattapat.su@ku.th>
2018-05-19 18:38:21 +0700

    add server

---
+++ server.js
@@ -0,0 +1,14 @@
+
+const express = require('express')
+const path = require('path')
+const port = process.env.PORT || 10001
+const app = express()
+
+app.use(express.static(__dirname))
+
+app.get('*', function (req, res) {
+    res.sendFile(path.resolve(__dirname, 'index.html'))
+})
+
+app.listen(port)
+console.log("Server started at port: " + port)
